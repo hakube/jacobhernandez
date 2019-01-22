b54abdb7ce7911af4a314a44439da2d4b0dd7d48
hakube <hakube@outlook.com>
2019-01-22 17:07:29 +0800

    Updated css

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,14 @@ downloadButton = document.getElementById('cv-dl-link');
 downloadButton.addEventListener('click', function(event) {
     alert('File not available at the moment');
 });
+
+//actions for hamburger menu
+
+hamburgerMenu = document.querySelector('.hamb-m');
+offScreenMenu = document.querySelector('.hidden');
+
+hamburgerMenu.addEventListener('click',
+    function(event) {
+        offScreenMenu.classList.toggle('hidden');
+    }
+);
