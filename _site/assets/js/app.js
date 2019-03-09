9f5abb0a2783dae084f0b0089778b5db0746e230
Jacob Hernandez <hakube@outlook.com>
2019-03-10 04:35:03 +0800

    Updated scripts and stylesheets

--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -1,11 +1,3 @@
-// actions for download button
-
-downloadButton = document.getElementById('cv-dl-link');
-
-downloadButton.addEventListener('click', function (event) {
-    alert('File not available at the moment');
-});
-
 //actions for hamburger menu
 
 hamburgerMenu = document.querySelector('.hamb-m');
